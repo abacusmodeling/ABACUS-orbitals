61ace737d4997f771f6146aefd6f25eaef09e0ab
kirk0830 <yike.huang@aliyun.com>
2024-09-26 14:30:10 +0800

    recover setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SIAB",
+    version="3.0-alpha",
+    author="ABACUS-AISI developers",
+    author_email="huangyk@aisi.ac.cn",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy<1.10", # required by some functionalities employing the old version simpson integral
+        "torch",
+        "torch_optimizer",
+        "torch_complex",
+        "addict"
+    ],
+    zip_safe=False,
+    classifiers=[
+        # Add classifiers to help others find your project
+        # Full list: https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Change as necessary
+        "Operating System :: OS Independent",
+    ],
+    scripts=[
+        "SIAB/SIAB_nouvelle.py",
+    ],
+   python_requires='<3.11', # required by SciPy 1.10
+)
