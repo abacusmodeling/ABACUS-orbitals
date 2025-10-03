c594e88526c634745ce1e85b7b8cdf403fba5255
linpz <linpz@mail.ustc.edu.cn>
2025-10-03 21:22:30 +0800

    1. add generate_jn() and generate_jn_smooth() in opt_orb_pytorch_dpsi

--- SIAB/opt_orb_pytorch_dpsi/IO/print_orbital.py
+++ SIAB/opt_orb_pytorch_dpsi/IO/print_orbital.py
@@ -9,11 +9,11 @@ periodtable = {   'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7,
                'Sn': 50, 'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55,
                'Ba': 56, #'La': 57, 'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61,
                     ## 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66, 'Ho': 67,
-                    ## 'Er': 68, 'Tm': 69, 'Yb': 70, 
-                    ## 'Lu': 71, 
+                    ## 'Er': 68, 'Tm': 69, 'Yb': 70,
+                    ## 'Lu': 71,
                'Hf': 72, 'Ta': 73,
                'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79,
-               'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 
+               'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83,
                     ## 'Po': 84, #'At': 85,
                     ## 'Rn': 86, #'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91,
                     ## 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97,
@@ -21,7 +21,7 @@ periodtable = {   'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7,
                     ## 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108,
                     ## 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112, 'Uut': 113,
                     ## 'Fl': 114, 'Uup': 115, 'Lv': 116, 'Uus': 117, 'Uuo': 118
-               } 
+               }
 
 def print_orbital(orb,info_element):
 	""" orb[it][il][iu][r] """
@@ -37,21 +37,23 @@ def print_orbital(orb,info_element):
 						print( '%.14e'%orb_tlur, end="  ",file=file)
 						if ir%4==3:	print(file=file)
 					print(file=file)
-					
-					
+
+
 def plot_orbital(orb,Rcut,dr):
 	for it,orb_t in orb.items():
 		#with open("orb_{0}_plot.dat".format(it),"w") as file:
 		with open("ORBITAL_PLOTU.dat", "w") as file:
 			Nr = int(Rcut[it]/dr[it])+1
 			for ir in range(Nr):
-				print( '%10.6f'%(ir*dr[it]),end="  ",file=file)
+				#print( '%10.6f'%(ir*dr[it]),end="  ",file=file)
+				print( ir*dr[it], end="\t",file=file)
 				for il,orb_tl in enumerate(orb_t):
 					for orb_tlu in orb_tl:
-						print( '%18.14f'%orb_tlu[ir],end="  ",file=file)
+						#print( '%18.14f'%orb_tlu[ir],end="  ",file=file)
+						print( orb_tlu[ir], end="\t",file=file)
 				print(file=file)
-				
-				
+
+
 def print_orbital_head(file,info_element,it):
 	print( "---------------------------------------------------------------------------", file=file )
 	print( "Element                     {0}".format(it), file=file )
@@ -67,4 +69,4 @@ def print_orbital_head(file,info_element,it):
 	print( "Mesh                        {0}".format(int(info_element[it].Rcut/info_element[it].dr)+1), file=file )
 	print( "dr                          {0}".format(info_element[it].dr), file=file )
 
-	
+
